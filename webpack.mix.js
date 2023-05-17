d8d3082950bfd781f8c8227a83f81c24c692a8f0
kibo13 <kimboris1310@gmail.com>
2023-05-18 00:46:18 +0600

    config: set start setting in webpack.mix.js file

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,17 +1,13 @@
 const mix = require('laravel-mix');
 
-/*
- |--------------------------------------------------------------------------
- | Mix Asset Management
- |--------------------------------------------------------------------------
- |
- | Mix provides a clean, fluent API for defining some Webpack build steps
- | for your Laravel applications. By default, we are compiling the CSS
- | file for the application as well as bundling up all the JS files.
- |
- */
+// auth
+mix .sass('resources/sass/auth/index.sass', 'public/css/auth.css')
+    .version()
 
-mix.js('resources/js/app.js', 'public/js')
-    .postCss('resources/css/app.css', 'public/css', [
-        //
-    ]);
+// admin
+// mix .js('resources/js/admin/index.js', 'public/js/admin.js')
+//     .sass('resources/sass/admin/index.sass', 'public/css/admin.css')
+//     .version()
+
+// vendors
+mix.copy('node_modules/bootstrap/dist/css/bootstrap.min.css', 'public/css/vendors/bootstrap.min.css')
