cfc3bf05143c2da0ee03430d6a8acf31636f4929
kibo13 <kimboris1310@gmail.com>
2023-05-19 00:42:05 +0600

    config: update settings in webpack.mix.js file

--- webpack.mix.js
+++ webpack.mix.js
@@ -9,9 +9,11 @@ mix .sass('resources/sass/auth/index.sass', 'public/css/auth.css')
     .version()
 
 // admin
-// mix .js('resources/js/admin/index.js', 'public/js/admin.js')
-//     .sass('resources/sass/admin/index.sass', 'public/css/admin.css')
-//     .version()
+mix .js('resources/js/admin/index.js', 'public/js/admin.js')
+    .sass('resources/sass/admin/index.sass', 'public/css/admin.css')
+    .version()
 
 // vendors
 mix.copy('node_modules/bootstrap/dist/css/bootstrap.min.css', 'public/css/vendors/bootstrap.min.css')
+mix.copy('node_modules/jquery/dist/jquery.min.js', 'public/js/vendors/jquery.min.js')
+mix.copy('node_modules/bootstrap/dist/js/bootstrap.min.js', 'public/js/vendors/bootstrap.min.js')
