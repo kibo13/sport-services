4cfb9da672a52d9b52ab5fc10ac90a32d4397ca2
kibo13 <kimboris1310@gmail.com>
2023-05-21 14:08:55 +0600

    refactor(user): update user seeder

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -17,15 +17,31 @@ class UserSeeder extends Seeder
      */
     public function run()
     {
-        User::query()->updateOrCreate([
-            'role_id'    => Role::ADMIN,
-            'name'       => 'Баймен',
-            'surname'    => 'Ермекбаев',
-            'patronymic' => 'Владимирович',
-            'birthday'   => '2003-06-20',
-            'phone'      => '7777777777',
-            'email'      => 'admin@sport.ru',
-            'password'   => Hash::make('tyler13'),
-        ]);
+        $users = [
+            [
+                'role_id'    => Role::ADMIN,
+                'name'       => 'Борис',
+                'surname'    => 'Ким',
+                'patronymic' => 'Юрьевич',
+                'birthday'   => '1990-10-13',
+                'phone'      => '7713404056',
+                'email'      => 'kimboris1310@gmail.com',
+                'password'   => Hash::make('secret'),
+            ],
+            [
+                'role_id'    => Role::ADMIN,
+                'name'       => 'Баймен',
+                'surname'    => 'Ермекбаев',
+                'patronymic' => 'Владимирович',
+                'birthday'   => null,
+                'phone'      => '7718203830',
+                'email'      => 'BaimenVip@mail.ru',
+                'password'   => Hash::make('secret'),
+            ]
+        ];
+
+        foreach ($users as $user) {
+            User::query()->updateOrCreate($user);
+        }
     }
 }
