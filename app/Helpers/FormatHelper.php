002c1d8f6ed24de472a09aa9cd0d1164b89a4e76
kibo13 <kimboris1310@gmail.com>
2023-05-21 22:07:05 +0600

    feat: add helper methods to format phone number

---
+++ app/Helpers/FormatHelper.php
@@ -0,0 +1,25 @@
+<?php
+
+function format_phone_number_for_storage(string $phoneNumber): string
+{
+    return preg_replace('/[^0-9]/', '', str_replace('+7', '', $phoneNumber));
+}
+
+function format_phone_number_for_display(string $phoneNumber): string
+{
+    // Удаление всех символов, кроме цифр
+    $phoneNumber = preg_replace('/\D/', '', $phoneNumber);
+
+    // Проверка наличия кода страны
+    if (strlen($phoneNumber) == 10) {
+        $countryCode = '+7';
+        $areaCode    = substr($phoneNumber, 0, 3);
+        $firstPart   = substr($phoneNumber, 3, 3);
+        $secondPart  = substr($phoneNumber, 6, 2);
+        $lastPart    = substr($phoneNumber, 8);
+
+        return "{$countryCode} {$areaCode} {$firstPart} {$secondPart} {$lastPart}";
+    }
+
+    return $phoneNumber;
+}
