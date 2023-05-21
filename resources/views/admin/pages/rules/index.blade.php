78eb4aaf4765ccfe88dc135509a4c70b93f2b59b
kibo13 <kimboris1310@gmail.com>
2023-05-22 01:13:30 +0600

    feat(rule): create rule controller, views

---
+++ resources/views/admin/pages/rules/index.blade.php
@@ -0,0 +1,37 @@
+@extends('admin.index')
+
+@section('title-admin', __('_section.rules'))
+
+@section('content-admin')
+    <section id="rules-index" class="bk-overflow-hidden">
+        <h3>{{ __('_section.rules') }}</h3>
+
+        <ul class="list-group">
+            <li class="list-group-item">
+                <a href="{{ asset('assets/rules/pool.docx') }}" download>
+                    Правила бассейна
+                </a>
+            </li>
+            <li class="list-group-item">
+                <a href="{{ asset('assets/rules/gym.docx') }}" download>
+                    Правила тренажёрного зала
+                </a>
+            </li>
+            <li class="list-group-item">
+                <a href="{{ asset('assets/rules/gym.docx') }}" download>
+                    Правила теннисных кортов
+                </a>
+            </li>
+            <li class="list-group-item">
+                <a href="{{ asset('assets/rules/gym.docx') }}" download>
+                    Правила спортзала
+                </a>
+            </li>
+            <li class="list-group-item">
+                <a href="{{ asset('assets/rules/gym.docx') }}" download>
+                    Правила безопасности
+                </a>
+            </li>
+        </ul>
+    </section>
+@endsection
