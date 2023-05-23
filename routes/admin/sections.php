6d8e6af1e658a20a77aa748914b3b5fac1db6021
kibo13 <kimboris1310@gmail.com>
2023-05-24 01:09:56 +0600

    refactor: move part of routes from web.php to separate sections.php

---
+++ routes/admin/sections.php
@@ -0,0 +1,30 @@
+<?php
+
+use App\Http\Controllers\Admin\ClientController;
+use App\Http\Controllers\Admin\HomeController;
+use App\Http\Controllers\Admin\ProfileController;
+use App\Http\Controllers\Admin\RuleController;
+use App\Http\Controllers\Admin\TrainerController;
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', [HomeController::class, 'index'])->name('home');
+
+Route::prefix('profile')->as('profile.')->group(function () {
+    Route::get('/', [ProfileController::class, 'index'])->name('index');
+    Route::match(['put', 'patch'], '/{user}', [ProfileController::class, 'update'])->name('update');
+    Route::post('/photo/update', [ProfileController::class, 'updatePhoto'])->name('update.photo');
+});
+
+Route::prefix('clients')->as('clients.')->group(function () {
+    Route::get('/', [ClientController::class, 'index'])->name('index');
+    Route::get('/{client}', [ClientController::class, 'show'])->name('show');
+});
+
+Route::prefix('trainers')->as('trainers.')->group(function () {
+    Route::get('/', [TrainerController::class, 'index'])->name('index');
+    Route::get('/{trainer}', [TrainerController::class, 'show'])->name('show');
+
+});
+
+Route::get('/rules', [RuleController::class, 'index'])->name('rules');
--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,5 @@
 <?php
 
-use App\Http\Controllers\Admin\HomeController;
-use App\Http\Controllers\Admin\ProfileController;
-use App\Http\Controllers\Admin\RuleController;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
@@ -20,21 +17,12 @@ Route::view('/mai', 'mai.index')->name('mai');
 
 // admin
 Route::middleware('auth')->group(function () {
-
-    Route::get('/', [HomeController::class, 'index'])->name('home');
-
-    Route::prefix('profile')->as('profile.')->group(function () {
-        Route::get('/', [ProfileController::class, 'index'])->name('index');
-        Route::match(['put', 'patch'], '/{user}', [ProfileController::class, 'update'])->name('update');
-        Route::post('/photo/update', [ProfileController::class, 'updatePhoto'])->name('update.photo');
-    });
-
-    Route::get('/rules', [RuleController::class, 'index'])->name('rules');
+    // Подключаем файл маршрутов sections.php
+    include_once __DIR__ . '/admin/sections.php';
 
     // Подключаем файл маршрутов users.php
     include_once __DIR__ . '/admin/users.php';
 
     // Подключаем файл маршрутов catalog.php
     include_once __DIR__ . '/admin/catalog.php';
-
 });
