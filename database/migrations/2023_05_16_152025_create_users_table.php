380f23d2e642c67be1f840a421545e5aff6814b1
kibo13 <kimboris1310@gmail.com>
2023-05-24 00:54:21 +0600

    database: add address column to users table

--- database/migrations/2023_05_16_152025_create_users_table.php
+++ database/migrations/2023_05_16_152025_create_users_table.php
@@ -27,6 +27,7 @@ class CreateUsersTable extends Migration
             $table->string('phone', 10)->nullable()->unique();
             $table->string('email')->unique();
             $table->string('photo')->nullable();
+            $table->string('address')->nullable();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->boolean('is_notify')->default(false);
