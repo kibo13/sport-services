4561ee932fbd9f0a720d8cd384e4f11e4aec4037
kibo13 <kimboris1310@gmail.com>
2023-05-23 23:35:00 +0600

    database: create migration to benefits table

---
+++ database/migrations/2023_05_22_184235_create_benefits_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateBenefitsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('benefits', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->decimal('discount', 4, 2);
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('benefits');
+    }
+}
