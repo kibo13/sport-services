8ac4be4eab14f35dbfc652582434c68941ccc9b7
kibo13 <kimboris1310@gmail.com>
2023-05-27 01:47:14 +0600

    database: create migration to passes table

---
+++ database/migrations/2023_05_26_172427_create_passes_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePassesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('passes', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('client_id')->constrained('users')->cascadeOnUpdate();
+            $table->foreignId('service_id')->constrained('services')->cascadeOnUpdate();
+            $table->date('start_date');
+            $table->date('end_date');
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('passes');
+    }
+}
