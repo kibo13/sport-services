069d669e6f06d164a5274443bed8b34f73fcba85
kibo13 <kimboris1310@gmail.com>
2023-05-28 00:00:46 +0600

    refactor(trainer): replace TrainerSeeder class on TrainerFactory class

---
+++ database/factories/TrainerFactory.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace Database\Factories;
+
+
+use App\Enums\Role;
+use App\Models\Specialization;
+use App\Models\User;
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
+
+class TrainerFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    protected $model = User::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition(): array
+    {
+        return [
+            'role_id'           => Role::INSTRUCTOR,
+            'name'              => $this->faker->firstName(),
+            'surname'           => $this->faker->lastName(),
+            'patronymic'        => $this->faker->firstNameMale(),
+            'birthday'          => $this->faker->dateTimeBetween('-35 years', '-25 years')->format('Y-m-d'),
+            'phone'             => $this->faker->unique()->numerify('771#######'),
+            'email'             => $this->faker->unique()->safeEmail(),
+            'address'           => $this->faker->streetAddress() . '-' . $this->faker->buildingNumber(),
+            'email_verified_at' => now(),
+            'password'          => Hash::make('secret'),
+            'remember_token'    => Str::random(10),
+        ];
+    }
+
+    /**
+     * Indicate that the model's email address should be unverified.
+     *
+     * @return Factory
+     */
+    public function unverified(): Factory
+    {
+        return $this->state(function (array $attributes) {
+            return [
+                'email_verified_at' => null,
+            ];
+        });
+    }
+
+    /**
+     * Define the callback to run after creating a user.
+     *
+     * @return $this
+     */
+    public function configure(): TrainerFactory
+    {
+        return $this->afterCreating(function (User $user) {
+            $user->specializations()->attach([
+                $this->getRandomSpecializationId()
+            ]);
+        });
+    }
+
+    /**
+     * Generate a random specialization ID.
+     *
+     * @return int
+     */
+    private function getRandomSpecializationId(): int
+    {
+        $service = Specialization::query()
+            ->inRandomOrder()
+            ->first();
+
+        return $service->id;
+    }
+}
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 
 use Database\Factories\ClientFactory;
+use Database\Factories\TrainerFactory;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -24,10 +25,10 @@ class DatabaseSeeder extends Seeder
             ServiceSeeder::class,
             BenefitSeeder::class,
             SpecializationSeeder::class,
-            TrainerSeeder::class,
         ]);
 
         // factories
+        TrainerFactory::new()->count(10)->create();
         ClientFactory::new()->count(30)->create();
     }
 }
