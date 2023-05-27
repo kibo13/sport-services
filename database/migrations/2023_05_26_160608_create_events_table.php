84ff50d44760f72d3ef25538f6e3b07c4aed1be5
kibo13 <kimboris1310@gmail.com>
2023-05-28 02:39:04 +0600

    database: update migration to events table

--- database/migrations/2023_05_26_160608_create_events_table.php
+++ database/migrations/2023_05_26_160608_create_events_table.php
@@ -1,7 +1,5 @@
 <?php
 
-use App\Models\Specialization;
-use App\Models\User;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -18,8 +16,8 @@ class CreateEventsTable extends Migration
         Schema::create('events', function (Blueprint $table) {
             $table->id();
             $table->string('title');
-            $table->foreignIdFor(Specialization::class)->constrained()->cascadeOnUpdate();
-            $table->foreignIdFor(User::class)->constrained()->cascadeOnUpdate();
+            $table->foreignId('specialization_id')->constrained('specializations')->cascadeOnUpdate();
+            $table->foreignId('trainer_id')->constrained('users')->cascadeOnUpdate();
             $table->date('start');
             $table->date('end');
             $table->string('place')->nullable();
