7c8fa08cf2b479701528a968c159f73550f2c362
kibo13 <kimboris1310@gmail.com>
2023-05-27 23:48:31 +0600

    database: create migration to specialization_user table

---
+++ database/migrations/2023_05_24_171620_create_specialization_user_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use App\Models\Specialization;
+use App\Models\User;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSpecializationUserTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('specialization_user', function (Blueprint $table) {
+            $table->foreignIdFor(Specialization::class)
+                ->constrained()
+                ->cascadeOnUpdate()
+                ->cascadeOnDelete();
+            $table->foreignIdFor(User::class)
+                ->constrained()
+                ->cascadeOnUpdate()
+                ->cascadeOnDelete();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('specialization_user');
+    }
+}
