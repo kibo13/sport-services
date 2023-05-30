241f82f3d000b93082198289cb1c20f7a46e1c07
kibo13 <kimboris1310@gmail.com>
2023-05-31 00:57:49 +0600

    config: update settings in webpack.mix.js

--- webpack.mix.js
+++ webpack.mix.js
@@ -29,6 +29,7 @@ mix .js([
 mix.copy('node_modules/font-awesome/css/font-awesome.min.css', 'public/css/vendors/font-awesome.min.css')
 mix.copy('node_modules/bootstrap/dist/css/bootstrap.min.css', 'public/css/vendors/bootstrap.min.css')
 mix.copy('node_modules/fullcalendar/main.min.css', 'public/css/vendors/fullcalendar.min.css')
+mix.copy('node_modules/select2/dist/css/select2.min.css', 'public/css/vendors/select2.min.css')
 mix.copy('node_modules/bootstrap/dist/js/bootstrap.min.js', 'public/js/vendors/bootstrap.min.js')
 mix.copy('node_modules/jquery/dist/jquery.min.js', 'public/js/vendors/jquery.min.js')
 mix.copy('node_modules/datatables.net/js/jquery.dataTables.min.js', 'public/js/vendors/jquery.dataTables.min.js')
@@ -36,3 +37,4 @@ mix.copy('node_modules/popper.js/dist/umd/popper.min.js', 'public/js/vendors/pop
 mix.copy('node_modules/moment/min/moment.min.js', 'public/js/vendors/moment.min.js')
 mix.copy('node_modules/fullcalendar/main.min.js', 'public/js/vendors/fullcalendar.min.js')
 mix.copy('node_modules/chart.js/dist/chart.min.js', 'public/js/vendors/chart.min.js')
+mix.copy('node_modules/select2/dist/js/select2.min.js', 'public/js/vendors/select2.min.js')
