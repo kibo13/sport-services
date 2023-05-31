69971eac2b62b4aaccb2a9ba16ed388600cf2e33
kibo13 <kimboris1310@gmail.com>
2023-05-31 23:34:31 +0600

    refactor(client): update client factory

--- database/factories/ClientFactory.php
+++ database/factories/ClientFactory.php
@@ -4,11 +4,14 @@ namespace Database\Factories;
 
 
 use App\Enums\Service\ServiceType;
+use App\Models\Benefit;
 use App\Models\Card;
 use App\Models\Service;
 use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Http\File;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Storage;
 use Illuminate\Support\Str;
 
 class ClientFactory extends Factory
@@ -28,6 +31,7 @@ class ClientFactory extends Factory
     public function definition(): array
     {
         return [
+            'benefit_id'        => $this->getRandomBenefitId(),
             'name'              => $this->faker->firstName(),
             'surname'           => $this->faker->lastName(),
             'patronymic'        => $this->faker->firstNameMale(),
@@ -38,6 +42,7 @@ class ClientFactory extends Factory
             'email_verified_at' => now(),
             'password'          => Hash::make('secret'),
             'remember_token'    => Str::random(10),
+            'certificate'       => $this->generateFile(),
         ];
     }
 
@@ -83,5 +88,36 @@ class ClientFactory extends Factory
             ->first();
 
         return $service->id;
+    }
+
+    /**
+     * Generate a random benefit ID.
+     *
+     * @return int
+     */
+    private function getRandomBenefitId(): int
+    {
+        $service = Benefit::query()
+            ->inRandomOrder()
+            ->first();
+
+        return $service->id;
+    }
+
+    /**
+     * Generate a random file and save it in the specified directory.
+     *
+     * @return string The generated file name.
+     */
+    protected function generateFile(): string
+    {
+        $templateFile = public_path('assets/images/certificate.jpg');
+        $destinationDirectory = 'certificates';
+        $fileName = $this->faker->uuid . '.jpg';
+        $file = new File($templateFile);
+
+        Storage::disk('public')->putFileAs($destinationDirectory, $file, $fileName);
+
+        return $destinationDirectory . '/' . $fileName;
     }
 }
