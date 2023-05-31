46ca33a7485d42a4d010d83bee9c958972b646e3
kibo13 <kimboris1310@gmail.com>
2023-06-01 01:19:53 +0600

    database: create migration to visits table

---
+++ database/migrations/2023_05_31_182838_create_payments_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePaymentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('payments', function (Blueprint $table) {
+            $table->id();
+            $table->integer('visit_id');
+            $table->integer('activity_id');
+            $table->foreignId('service_id')->nullable(false)->constrained('services')->cascadeOnUpdate();
+            $table->foreignId('client_id')->nullable(true)->constrained('users')->cascadeOnUpdate();
+            $table->double('amount');
+            $table->boolean('is_paid')->default(false);
+            $table->date('paid_at')->nullable();
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('payments');
+    }
+}
