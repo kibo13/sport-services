4a09ab64f815ab9c5aaf7b655c66eafcf052b331
kibo13 <kimboris1310@gmail.com>
2023-06-03 20:29:50 +0600

    config: add settings to consoletvs/charts require

--- config/app.php
+++ config/app.php
@@ -166,6 +166,7 @@ return [
          * Package Service Providers...
          */
         Maatwebsite\Excel\ExcelServiceProvider::class,
+        ConsoleTVs\Charts\ChartsServiceProvider::class,
 
         /*
          * Application Service Providers...
