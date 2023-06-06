33d29185298d89048638aa8c1c859b7a8d5ed449
kibo13 <kimboris1310@gmail.com>
2023-06-06 17:56:39 +0600

    refactor(card): update index method in CardLessonController class

--- app/Http/Controllers/Admin/CardLessonController.php
+++ app/Http/Controllers/Admin/CardLessonController.php
@@ -4,12 +4,23 @@ namespace App\Http\Controllers\Admin;
 
 
 use App\Http\Controllers\Controller;
+use App\Models\Activity;
+use App\Repositories\Card\CardRepositoryInterface;
 use Illuminate\Http\Request;
 
 class CardLessonController extends Controller
 {
-    public function index()
+    public function index(CardRepositoryInterface $cardRepository)
     {
-        return view('admin.pages.lessons.index');
+        $user = auth()->user();
+        $activities = Activity::all();
+        $ids = $activities->pluck('id')->toArray();
+        $cards = $cardRepository->getActiveCards($ids, $user->id);
+
+        return view('admin.pages.lessons.index', [
+            'user' => $user,
+            'activities' => $activities,
+            'cards' => $cards
+        ]);
     }
 }
