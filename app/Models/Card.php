0640694de0e86e76e3c8c1bffb3f07759fa9cb2f
kibo13 <kimboris1310@gmail.com>
2023-06-06 17:56:10 +0600

    feat(card): update fillable property and add relationships in model

--- app/Models/Card.php
+++ app/Models/Card.php
@@ -6,6 +6,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Card extends Model
@@ -14,7 +15,9 @@ class Card extends Model
 
     protected $fillable = [
         'client_id',
+        'activity_id',
         'service_id',
+        'is_active',
     ];
 
     protected $dates = [
@@ -36,8 +39,18 @@ class Card extends Model
         return $this->belongsTo(User::class, 'client_id');
     }
 
+    public function activity(): BelongsTo
+    {
+        return $this->belongsTo(Activity::class);
+    }
+
     public function service(): BelongsTo
     {
         return $this->belongsTo(Service::class);
+    }
+
+    public function lessons(): HasMany
+    {
+        return $this->hasMany(CardLesson::class);
     }
 }
