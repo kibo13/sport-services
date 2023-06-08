729d03bac28fd9675b7fe4337c6f16baf74fd18d
kibo13 <kimboris1310@gmail.com>
2023-06-09 01:07:25 +0600

    feat: create sms service class and job to send message

---
+++ app/Jobs/Client/SendMessageJob.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace App\Jobs\Client;
+
+
+use App\Services\Sms\SmsService;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldBeUnique;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Support\Facades\Log;
+
+class SendMessageJob implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    /**
+     * The number of times the job may be attempted.
+     *
+     * @var int
+     */
+    public $tries = 2;
+
+    /**
+     * The phone number of the recipient.
+     *
+     * @var string
+     */
+    protected $phone;
+
+    /**
+     * The message content to be sent.
+     *
+     * @var string
+     */
+    protected $message;
+
+    /**
+     * Create a new job instance.
+     *
+     * @return void
+     */
+    public function __construct($phone, $message)
+    {
+        $this->phone = $phone;
+        $this->message = $message;
+    }
+
+    /**
+     * Execute the job.
+     *
+     * @return void
+     */
+    public function handle()
+    {
+        SmsService::send($this->phone, $this->message);
+        Log::info('Message has been successfully sent');
+    }
+}
---
+++ app/Services/Sms/SmsService.php
@@ -0,0 +1,53 @@
+<?php
+
+
+namespace App\Services\Sms;
+
+
+use GuzzleHttp\Client;
+use GuzzleHttp\Exception\GuzzleException;
+use Illuminate\Http\JsonResponse;
+
+class SmsService
+{
+    private const BASE_URL = 'https://smsc.kz/sys/send.php';
+    private const USERNAME = 'Baimen';
+    private const PASSWORD = 'Qwerty123';
+
+    public static function send(string $phone, string $message): JsonResponse
+    {
+        try {
+            $client = new Client();
+            $response = $client->get(self::BASE_URL, [
+                'query' => [
+                    'login'  => self::USERNAME,
+                    'psw'    => self::PASSWORD,
+                    'phones' => '7' . $phone,
+                    'mes'    => $message
+                ]
+            ]);
+
+            $body = $response->getBody()->getContents();
+            $statuses = [
+                'status=100',
+                'status=101',
+                'status=102',
+            ];
+            if (in_array($body, $statuses)) {
+                return response()->json([
+                    'status' => 'PENDING'
+                ], 200);
+            } else {
+                return response()->json([
+                    'status' => 'error',
+                    'data' => $body
+                ], 400);
+            }
+        } catch (GuzzleException $exception) {
+            return response()->json([
+                'status' => 'error',
+                'message' => $exception->getMessage()
+            ], 500);
+        }
+    }
+}
