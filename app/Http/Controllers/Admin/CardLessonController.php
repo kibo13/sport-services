9c5269169169b6d446a5686790c2102c7469cacc
kibo13 <kimboris1310@gmail.com>
2023-06-09 01:10:01 +0600

    refactor(card-lesson): implement method to send sms to client in update method of CardLessonController class

--- app/Http/Controllers/Admin/CardLessonController.php
+++ app/Http/Controllers/Admin/CardLessonController.php
@@ -4,9 +4,11 @@ namespace App\Http\Controllers\Admin;
 
 
 use App\Http\Controllers\Controller;
+use App\Jobs\Client\SendMessageJob;
 use App\Models\Activity;
 use App\Models\Card;
 use App\Models\CardLesson;
+use App\Models\User;
 use App\Repositories\Card\CardRepositoryInterface;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
@@ -44,6 +46,24 @@ class CardLessonController extends Controller
     {
         $lesson->update($request->all());
 
+        $card = $lesson->card;
+        $client = $card->client;
+
+        $this->sendLessonNotification($client, $card);
+
         return redirect()->back();
+    }
+
+    private function sendLessonNotification(User $client, Card $card)
+    {
+        if ($client->is_notify) {
+            $activity = $card->activity->name;
+            $expirationDate = format_date_for_display($card->end);
+            $remainingLessonsCount = $card->getRemainingLessonsCount();
+            $phone = $client->phone;
+            $message = "Здравствуйте! Вы посетили занятие ($activity), у вас осталось $remainingLessonsCount посещений до $expirationDate года";
+
+            dispatch(new SendMessageJob($phone, $message));
+        }
     }
 }
