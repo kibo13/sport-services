26fd2e6c4c87f7751b1c4f7b0482c88d34bf7f10
kibo13 <kimboris1310@gmail.com>
2023-06-10 01:02:30 +0600

    database: add education_id, experience, note columns to users table

--- database/migrations/2023_05_16_152025_create_users_table.php
+++ database/migrations/2023_05_16_152025_create_users_table.php
@@ -1,7 +1,5 @@
 <?php
 
-use App\Models\Benefit;
-use App\Models\Role;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -17,8 +15,9 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->foreignIdFor(Role::class)->nullable(false)->constrained()->cascadeOnUpdate();
-            $table->foreignIdFor(Benefit::class)->nullable(true)->constrained()->cascadeOnUpdate();
+            $table->foreignId('role_id')->nullable(false)->constrained('roles')->cascadeOnUpdate();
+            $table->foreignId('benefit_id')->nullable(true)->constrained('benefits')->cascadeOnUpdate();
+            $table->foreignId('education_id')->nullable(true)->constrained('educations')->cascadeOnUpdate();
             $table->string('name');
             $table->string('surname');
             $table->string('patronymic')->nullable();
@@ -35,6 +34,8 @@ class CreateUsersTable extends Migration
             $table->string('password');
             $table->boolean('is_notify')->default(false);
             $table->boolean('is_hidden')->default(false);
+            $table->smallInteger('experience')->nullable();
+            $table->text('note')->nullable();
             $table->string('certificate')->nullable();
             $table->rememberToken();
             $table->softDeletes();
