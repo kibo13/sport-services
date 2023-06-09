aa87edd4495f85c3297b1e79a901d6560fe86e6e
kibo13 <kimboris1310@gmail.com>
2023-06-09 09:43:17 +0600

    feat(card-lesson): create enum LessonCount class

---
+++ app/Enums/Lesson/LessonCount.php
@@ -0,0 +1,34 @@
+<?php
+
+
+namespace App\Enums\Lesson;
+
+
+class LessonCount
+{
+    public const LESSON_1  = 1;
+    public const LESSON_2  = 2;
+    public const LESSON_3  = 3;
+    public const LESSON_4  = 4;
+    public const LESSON_5  = 5;
+    public const LESSON_6  = 6;
+    public const LESSON_7  = 7;
+    public const LESSON_8  = 8;
+    public const LESSON_9  = 9;
+    public const LESSON_10 = 10;
+    public const LESSON_11 = 11;
+
+    public const TEXTS = [
+        self::LESSON_1  => '1 занятие',
+        self::LESSON_2  => '2 занятия',
+        self::LESSON_3  => '3 занятия',
+        self::LESSON_4  => '4 занятия',
+        self::LESSON_5  => '5 занятий',
+        self::LESSON_6  => '6 занятий',
+        self::LESSON_7  => '7 занятий',
+        self::LESSON_8  => '8 занятий',
+        self::LESSON_9  => '9 занятий',
+        self::LESSON_10 => '10 занятий',
+        self::LESSON_11 => '11 занятий',
+    ];
+}
