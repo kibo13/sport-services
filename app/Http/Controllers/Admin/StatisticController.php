2a24e165422751c615adbee04c5e05f94be03abc
kibo13 <kimboris1310@gmail.com>
2023-06-21 15:08:28 +0600

    feat(statistic): add statistics on number of clients with trainers

--- app/Http/Controllers/Admin/StatisticController.php
+++ app/Http/Controllers/Admin/StatisticController.php
@@ -5,10 +5,12 @@ namespace App\Http\Controllers\Admin;
 
 use App\Charts\MasterChart;
 use App\Http\Controllers\Controller;
+use App\Models\User;
 use App\Repositories\Activity\ActivityRepositoryInterface;
 use App\Repositories\Card\CardLessonRepositoryInterface;
 use App\Repositories\Event\EventRepositoryInterface;
 use App\Repositories\Payment\PaymentRepositoryInterface;
+use App\Repositories\Trainer\TrainerRepositoryInterface;
 use App\Services\Chart\ChartService;
 use Carbon\Carbon;
 use Illuminate\Http\Request;
@@ -19,18 +21,21 @@ class StatisticController extends Controller
     protected $activityRepository;
     protected $eventRepository;
     protected $cardLessonRepository;
+    protected $trainerRepository;
 
     public function __construct(
         ActivityRepositoryInterface $activityRepository,
         PaymentRepositoryInterface $paymentRepository,
         EventRepositoryInterface $eventRepository,
-        CardLessonRepositoryInterface $cardLessonRepository
+        CardLessonRepositoryInterface $cardLessonRepository,
+        TrainerRepositoryInterface $trainerRepository
     )
     {
         $this->activityRepository = $activityRepository;
         $this->paymentRepository = $paymentRepository;
         $this->eventRepository = $eventRepository;
         $this->cardLessonRepository = $cardLessonRepository;
+        $this->trainerRepository = $trainerRepository;
     }
 
     public function index(Request $request)
@@ -39,63 +44,91 @@ class StatisticController extends Controller
         $till = $request->input('till', Carbon::now()->endOfMonth()->format('Y-m-d'));
         $labels = ChartService::generateLabels($from, $till);
         $activities = $this->activityRepository->getAll();
-        $activityPayments = $this->initializeActivityData($activities, $labels);
-        $activityEvents = $this->initializeActivityData($activities, $labels);
-        $activityLessons = $this->initializeActivityData($activities, $labels);
+        $trainers = $this->trainerRepository->getAll();
+        $activityPayments = $this->initializeData($activities, $labels);
+        $activityEvents = $this->initializeData($activities, $labels);
+        $activityLessons = $this->initializeData($activities, $labels);
+        $trainerClients = $this->initializeData($trainers, $labels);
 
         foreach ($activities as $activity) {
             $payments = $this->paymentRepository->getPaymentsByActivity($activity->id, $from, $till);
             $events = $this->eventRepository->getEventsByActivity($activity->id, $from, $till);
             $lessons = $this->cardLessonRepository->getLessonsByActivity($activity->id, $from, $till);
 
-            $this->updateActivityData($activityPayments[$activity->id], $payments);
-            $this->updateActivityData($activityEvents[$activity->id], $events);
-            $this->updateActivityData($activityLessons[$activity->id], $lessons);
+            $this->updateData($activityPayments[$activity->id], $payments);
+            $this->updateData($activityEvents[$activity->id], $events);
+            $this->updateData($activityLessons[$activity->id], $lessons);
         }
 
-        $paymentChart = $this->createChart($labels, $activityPayments);
-        $eventChart = $this->createChart($labels, $activityEvents);
-        $lessonChart = $this->createChart($labels, $activityLessons);
+        foreach ($trainers as $trainer) {
+            $clients = $this->trainerRepository->getClientCountByTrainer($trainer->id, $from, $till);
+
+            $this->updateData($trainerClients[$trainer->id], $clients);
+        }
+
+        $paymentChart = $this->createActivityChart($labels, $activityPayments);
+        $eventChart = $this->createActivityChart($labels, $activityEvents);
+        $lessonChart = $this->createActivityChart($labels, $activityLessons);
+        $trainerChart = $this->createTrainerChart($labels, $trainerClients);
 
         return view('admin.pages.statistics.index', [
             'paymentChart' => $paymentChart,
             'eventChart' => $eventChart,
-            'lessonChart' => $lessonChart
+            'lessonChart' => $lessonChart,
+            'trainerChart' => $trainerChart,
         ]);
     }
 
-    private function initializeActivityData($activities, $labels): array
+    private function initializeData($resources, $labels): array
     {
-        $activityData = [];
+        $data = [];
 
-        foreach ($activities as $activity) {
-            $activityData[$activity->id] = array_fill_keys($labels, 0);
+        foreach ($resources as $resource) {
+            $data[$resource->id] = array_fill_keys($labels, 0);
         }
 
-        return $activityData;
+        return $data;
     }
 
-    private function updateActivityData(&$activityData, $items)
+    private function updateData(&$data, $items)
     {
         foreach ($items as $item) {
-            if (array_key_exists($item->period, $activityData)) {
-                $activityData[$item->period] = $item->count;
+            if (array_key_exists($item->period, $data)) {
+                $data[$item->period] = $item->count;
             }
         }
     }
 
-    private function createChart($labels, $activityData): MasterChart
+    private function createActivityChart($labels, $activityData): MasterChart
     {
         $chart = new MasterChart;
         $chart->labels($labels);
 
-        foreach ($activityData as $activityId => $activity) {
+        foreach ($activityData as $activityId => $activityValues) {
             $activityName = $this->activityRepository->getNameActivityById($activityId);
             $activityColor = $this->activityRepository->getColorActivityById($activityId);
 
-            $chart->dataset($activityName, 'bar', array_values($activity))
+            $chart->dataset($activityName, 'bar', array_values($activityValues))
                 ->options([
                     'backgroundColor' => $activityColor,
+                    'borderColor' => 'transparent',
+                ]);
+        }
+
+        return $chart;
+    }
+
+    private function createTrainerChart($labels, $trainerData): MasterChart
+    {
+        $chart = new MasterChart;
+        $chart->labels($labels);
+
+        foreach ($trainerData as $trainerId => $trainerValues) {
+            $trainer = User::query()->find($trainerId);
+
+            $chart->dataset($trainer->short_name, 'bar', array_values($trainerValues))
+                ->options([
+                    'backgroundColor' => generate_color(),
                     'borderColor' => 'transparent',
                 ]);
         }
---
+++ resources/views/admin/pages/statistics/tabs/trainers.blade.php
@@ -0,0 +1,5 @@
+<h4>Статистика по тренерам</h4>
+<div>
+    {{ $trainerChart->container() }}
+    {{ $trainerChart->script() }}
+</div>
