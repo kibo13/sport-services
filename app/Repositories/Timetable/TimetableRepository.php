47c767d2659a4c2dbf122d321d3bfe946620ffc9
kibo13 <kimboris1310@gmail.com>
2023-06-25 23:32:55 +0600

    feat(timetable): add getAllLessons method to repository class and interface

--- app/Repositories/Timetable/TimetableRepository.php
+++ app/Repositories/Timetable/TimetableRepository.php
@@ -6,6 +6,7 @@ namespace App\Repositories\Timetable;
 
 use App\Models\Timetable;
 use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Support\Facades\DB;
 
 class TimetableRepository implements TimetableRepositoryInterface
 {
@@ -17,6 +18,29 @@ class TimetableRepository implements TimetableRepositoryInterface
     public function getAll()
     {
         return $this->createQuery()->get();
+    }
+
+    public function getAllLessons()
+    {
+        return $this->createQuery()
+            ->join('activities', 'activities.id', 'timetables.activity_id')
+            ->join('groups', 'groups.id', 'timetables.group_id')
+            ->join('users', 'users.id', 'timetables.trainer_id')
+            ->select([
+                'activities.id as activity_id',
+                'activities.name as activity',
+                'groups.name as group',
+                'groups.color as bgColor',
+                'users.full_name as trainer',
+                'timetables.id as timetable_id',
+                'timetables.start',
+                'timetables.end',
+                DB::raw('CONCAT_WS(\'-\', DATE_FORMAT(timetables.start, \'%H:%i\'), DATE_FORMAT(timetables.end, \'%H:%i\')) as title'),
+                'timetables.trainer_id',
+                'timetables.is_replace',
+                'timetables.note',
+            ])
+            ->get();
     }
 
     public function getLessonsByGroupAndDate(array $groupIds, $month, $year)
--- app/Repositories/Timetable/TimetableRepositoryInterface.php
+++ app/Repositories/Timetable/TimetableRepositoryInterface.php
@@ -7,5 +7,6 @@ namespace App\Repositories\Timetable;
 interface TimetableRepositoryInterface
 {
     public function getAll();
+    public function getAllLessons();
     public function getLessonsByGroupAndDate(array $groupIds, $month, $year);
 }
