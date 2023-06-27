cf090f53a7f9adab74be17829b49f246b8c4fcb4
kibo13 <kimboris1310@gmail.com>
2023-06-27 16:09:45 +0600

    database: create migration to methods table

---
+++ database/migrations/2023_06_26_182616_create_methods_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMethodsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('methods', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('activity_id')->constrained('activities')->cascadeOnUpdate();
+            $table->integer('number');
+            $table->unique(['activity_id', 'number']);
+            $table->text('note')->nullable();
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('methods');
+    }
+}
